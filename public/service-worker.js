7068e0bd94b113bbbd1247697ba205e942340eff
Staci Genesi <sgenesi@hotmail.com>
2021-03-28 15:18:01 -0600

    working with the caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,8 +15,8 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v3";
+const DATA_CACHE_NAME = "data-cache-v2";
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
